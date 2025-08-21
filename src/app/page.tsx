b40a9e1a321d89f901b43d61f2f6c56e640909eb
ChristianKiernan <184921820+ChristianKiernan@users.noreply.github.com>
2025-08-21 06:48:09 -0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Page() {
+  return (
+    <main>
+        <header>
+          <h1>
+            Welcome to the App
+          </h1>
+        </header>
+    </main>
+  );
+}
